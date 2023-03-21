7ce066c06aa72ff5bf0ec2ae9a2de3902ed73f6c
Gilvano Piontkoski <g.piontkoski@gmail.com>
2023-03-21 08:55:25 -0300

    feat: Add coroutine

---
+++ concorrencia/goroutine/goroutine.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fale(pessoa, texto string, qtde int) {
+	for i := 0; i < qtde; i++ {
+		time.Sleep(time.Second)
+		fmt.Printf("%s: %s (iteração %d)\r", pessoa, texto, i+1)
+	}
+}
+
+func main() {
+	// fale("Maria", "Pq vc não fala comigo?", 3)
+	// fale("João", "Só posso falar depois de vc!", 1)
+
+	go fale("Maria", "Ei...", 10)
+	fale("João", "Opa...", 5)
+
+}
